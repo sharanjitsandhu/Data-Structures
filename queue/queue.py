f5a2c2ddb4ee46f601a9270a1cf85cafa82cba3c
Sharanjit Sandhu <sharanjit.sandhuu@gmail.com>
2019-07-23 13:26:52 -0700

    Updated queue file

--- queue/queue.py
+++ queue/queue.py
@@ -6,14 +6,17 @@ class Queue:
         self.storage = []
 
     def enqueue(self, item):
+        '''enqueue should add an item to the back of the queue'''
         return self.storage.append(item)
 
     def dequeue(self):
+        '''dequeue should remove and return an item from the front of the queue'''
         if self.storage:
             return self.storage.pop(0)
 
     def len(self):
+        '''len returns the number of items in the queue'''
         return len(self.storage)
 
 
-''' adding and removing to the end using Lists '''
+''' adding and removing item using Lists '''
